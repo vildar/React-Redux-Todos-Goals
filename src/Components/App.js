d7d23d7ca7b9111c2becc0137ad4435d7ce0824a
Hanumanth P <48819979+vildar@users.noreply.github.com>
2020-05-23 12:51:20 +0530

    Migrated action functions

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from './node_modules/react'
 
 function App() {
   return (
@@ -8,4 +8,4 @@ function App() {
   )
 }
 
-export default App;
+export default App
